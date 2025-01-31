a6139a9120ed12fc4a3bcaa11a3765f15682234d
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2025-01-31 15:18:27 +0530

    admin project

---
+++ src/main/java/com/drucare/admin/AdminController.java
@@ -0,0 +1,14 @@
+package com.drucare.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+	
+	@GetMapping("/admin")
+	public String adminService() {
+		return "Welcome to Admin Service";
+	}
+
+}
---
+++ src/main/java/com/drucare/admin/AdminProjectApplication.java
@@ -0,0 +1,13 @@
+package com.drucare.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminProjectApplication.class, args);
+	}
+
+}
